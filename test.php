f74f8cd1a17ef192c6d96e734dacba08242f0269
alb-ctrl <esekiel.98@icloud.com>
2021-09-30 15:04:46 -0400

    set full path

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-require ("../../dbconfig.php");
+require ("/home/bitnami/dbconfig.php");
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR
 	die('Coul not connect MySQL: ' . mysqli_connect_error () );
 
