bd19f244813a7f87eb430df744bce3d5573879f9
alb-ctrl <esekiel.98@icloud.com>
2021-09-30 15:01:19 -0400

    add db name

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 
 require ("/home/bitnami/dbconfig.php");
-$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD) OR
+$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR
 	die('Coul not connect MySQL: ' . mysqli_connect_error () );
 
 if($dbc) echo "we did it";
