ff97dba247926e28b153af5437a7c13b0c3fbf5e
alb-ctrl <esekiel.98@icloud.com>
2021-10-07 12:15:21 -0400

    fix path

--- PizzaPlanet/login.php
+++ PizzaPlanet/login.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 <title>Login</title>
-<link rel="stylesheet" type="text/css" href="login_style.css">
+<link rel="stylesheet" type="text/css" href="../rsrc/styles/login_style.css">
 
 <!-- Bootstrap Scripts -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
