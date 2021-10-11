a7702b2b01e047e47e4c02799d272c7c39693f73
verdecit <verdecit@kean.edu>
2021-10-11 12:47:03 -0400

    update few changes to header links

--- PizzaPlanet/login.php
+++ PizzaPlanet/login.php
@@ -7,6 +7,7 @@
 
 <link rel="stylesheet" href="../rsrc/styles/index_styles.css">
 <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/de6bb176d6.js" crossorigin="anonymous"></script>
 
 <!-- Bootstrap Scripts -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
