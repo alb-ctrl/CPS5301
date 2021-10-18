970b52ade6a0087440147259b8b991f69b5567d4
alb-ctrl <esekiel.98@icloud.com>
2021-10-18 11:27:29 -0400

    testing

--- PizzaPlanet/cart.php
+++ PizzaPlanet/cart.php
@@ -6,7 +6,7 @@ $item_id = $_POST['menu_id'];
 $quantity = 1;
 $index = count($_SESSION['cart']);
 $_SESSION['cart'][$index+1] = array('menu_id' => $item_id, 'quantity' => $quantity);
-foreach($_SESSION['cart'] as $key => $value){
+foreach($_SESSION['cart'][0] as $value){
     echo "{$value}";
 }
 ?>
