c0cb4aa3615eb08703fa78a7e76691cb2e89a07f
alb-ctrl <esekiel.98@icloud.com>
2021-10-19 15:22:45 -0400

    debuging

--- PizzaPlanet/cart.php
+++ PizzaPlanet/cart.php
@@ -4,6 +4,7 @@ if (!isset($_SESSION['cart'])){
     $_SESSION['cart'] = array();
 }
 $item_id = $_POST['menu_id'];
+echo "Menu id: $item_id";
 $quantity = 1;
 $index = count($_SESSION['cart']);
 $_SESSION['cart'][$index+1] = array('menu_id' => $item_id, 'quantity' => $quantity);
