42d47bd12e15e99e79d8d74c902afa5c8b460f70
alb-ctrl <esekiel.98@icloud.com>
2021-10-19 19:47:07 -0400

    view cart

--- PizzaPlanet/cart.php
+++ PizzaPlanet/cart.php
@@ -10,7 +10,9 @@ if (!isset($_SESSION['cart'])){
     $quantity = 1;
     $index = count($_SESSION['cart']);
     $_SESSION['cart'][$index+1] = array('menu_id' => $item_id, 'quantity' => $quantity);
-    
+    foreach($_SESSION['cart'] as $value){
+        echo "<br>it should work<br>".$value['quantity'];
+    }
     print_r($_SESSION['cart']);
     // this is how you report an error to ajax
     //header("HTTP/1.0 500 You're not logged in. Try loggin in first");
