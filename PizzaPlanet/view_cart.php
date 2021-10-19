7bfd86840b5b02781697d266f613ac6e13a23fff
alb-ctrl <esekiel.98@icloud.com>
2021-10-19 19:52:28 -0400

    view cart

--- PizzaPlanet/view_cart.php
+++ PizzaPlanet/view_cart.php
@@ -5,7 +5,6 @@
     <title>Cart</title>
     <link rel="stylesheet" type="text/css" href="../rsrc/styles/cart_style.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,900" rel="stylesheet">
-
 </head>
 
 <body>
@@ -36,7 +35,27 @@
                 <div class="remove"><u>Remove</u></div>
             </div>
         </div>
-        
+
+        <div class="Cart-Items pad">
+            <div class="image-box">
+                <img src="images/grapes.png" style= 'height:"120px"'/>
+            </div>
+            <div class="about">
+                <h1 class="title">Grapes Juice</h1>
+                <h3 class="subtitle">250ml</h3>
+                <img src="images/veg.png" style='height:"30px" ' />
+            </div>
+            <div class="counter">
+                <div class="btn">+</div>
+                <div class="count">1</div>
+                <div class="btn">-</div>
+            </div>
+            <div class="prices">
+                <div class="amount">$3.19</div>
+                <div class="save"><u>Save for later</u></div>
+                <div class="remove"><u>Remove</u></div>
+            </div>
+        </div>
         <hr>
         <div class="checkout">
             <div class="total">
