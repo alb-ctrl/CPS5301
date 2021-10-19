2817aace96677893a842b2c2761486cf03efb41b
alb-ctrl <esekiel.98@icloud.com>
2021-10-18 22:25:27 -0400

    testing

--- PizzaPlanet/cart.php
+++ PizzaPlanet/cart.php
@@ -7,7 +7,7 @@ $quantity = 1;
 $index = count($_SESSION['cart']);
 $_SESSION['cart'][$index+1] = array('menu_id' => $item_id, 'quantity' => $quantity);
 foreach($_SESSION['cart'] as $value){
-    echo "<br>cart<br>".$value['quantity'];
+    echo "<br>Cart<br>".$value['quantity'];
 }
 //print_r($_SESSION['cart']);
 // this is how you report an error to ajax
