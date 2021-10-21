a3f609f14ec34447ceb86e28f347e51a66681918
alb-ctrl <esekiel.98@icloud.com>
2021-10-21 12:26:53 -0400

    old quantity

--- PizzaPlanet/cart.php
+++ PizzaPlanet/cart.php
@@ -35,6 +35,7 @@ if (isset($_POST['remove_item'])) {
 if (isset($_POST['increase_quantity'])) {
     foreach($_SESSION['cart'] as $value){
         if ($value['cart_index'] == $_POST['cart_index']){
+            echo "old quantity ". $value['quantity'];
             $value['quantity']+=1;
             print_r($value);
         }
