cfab68d24edf53507f2e8d72880977525a996b1c
verdecit <verdecit@kean.edu>
2021-11-07 22:48:03 -0500

    2fa different approach

--- PizzaPlanet/2fa-email-func.php
+++ PizzaPlanet/2fa-email-func.php
@@ -2,24 +2,24 @@
 require ("/home/bitnami/dbconfig.php");
 $db = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR
 	die('Coul not connect MySQL: ' . mysqli_connect_error () );
-$v_email = $_POST['v_email'];
+// $v_email = $_POST['v_email'];
 
-$v_sql = "SELECT * FROM users WHERE email='$v_email' LIMIT 1";
-$v_result = mysqli_query($db, $v_sql);
+// $v_sql = "SELECT * FROM users WHERE email='$v_email' LIMIT 1";
+// $v_result = mysqli_query($db, $v_sql);
 
-if (mysqli_num_rows($v_result) == 0) {
-    echo "email is not associated with any registered account";
-    }
+// if (mysqli_num_rows($v_result) == 0) {
+//     echo "email is not associated with any registered account";
+//     }
 
-else if (mysqli_num_rows($v_result) > 0) {
-sendEmail($v_email);
+// else if (mysqli_num_rows($v_result) > 0) {
+// sendEmail($v_email);
 
-}
+// }
 
 
-function sendEmail($v_email)
+function sendAEmail($v_email, $code)
     {
-        $v_emailBody = 'Hello user: '.$v_email.' Please click link to enter your account http://3.82.35.248/CPS5301/PizzaPlanet/2FA.php ';
+        $v_emailBody = 'Hello user: '.$v_email.' code: '.$code;
 
         $v_body = '{
             "subject": "From Pizza Planet",
--- PizzaPlanet/mysqli_connect_Register.php
+++ PizzaPlanet/mysqli_connect_Register.php
@@ -129,7 +129,10 @@ if (isset($_POST['login_user']))
         //use 2FA to verify login
         if (mysqli_num_rows($result1) == 1) 
         {
-          echo $email_row[0];
+            $code = 123;
+            include('2fa-email-func.php');
+            //2fa email function name change
+            sendAEmail($email_row[0], $code);
           
         }
         //if user forgets password, let them log in and direct them to reset_password.html
