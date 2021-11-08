f179e1f3f8807c3d44bf28ceca0b66deac218059
verdecit <verdecit@kean.edu>
2021-11-07 22:54:04 -0500

    t

--- PizzaPlanet/2fa-email-func.php
+++ PizzaPlanet/2fa-email-func.php
@@ -17,9 +17,9 @@ $db = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR
 // }
 
 
-function sendAEmail($v_email, $code)
+function sendAEmail($v_email)
     {
-        $v_emailBody = 'Hello user: '.$v_email.' code: '.$code.'';
+        $v_emailBody = 'Hello user: '.$v_email.' code: ';
 
         $v_body = '{
             "subject": "From Pizza Planet",
