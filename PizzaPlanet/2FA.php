0fe1f7bd2a4b7af7f4669dfa1d7e01b4c0f9698d
verdecit <verdecit@kean.edu>
2021-11-09 01:25:10 -0500

    t

--- PizzaPlanet/2FA.php
+++ PizzaPlanet/2FA.php
@@ -2,9 +2,9 @@
 require ("/home/bitnami/dbconfig.php");
 // page will set the session variables after the user has clicked the link in the email
 // sent to them and redirect them to the index page with their session variables set
-
+session_start();
 echo $_SESSION["code"];
-echo "\nsession above post below\n";
+echo "\n<-session above post below->\n";
 echo $_POST["scode"];
 if(isset($_SESSION["code"])){
     $scode = $_POST["scode"];
