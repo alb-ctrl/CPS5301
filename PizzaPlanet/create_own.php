1b37e824098629104548cc440b82eedd238c1071
alb-ctrl <esekiel.98@icloud.com>
2021-12-02 12:54:42 -0500

    fix

--- PizzaPlanet/create_own.php
+++ PizzaPlanet/create_own.php
@@ -94,7 +94,7 @@ else{
         while ($row = mysqli_fetch_array($results)) {
 
         echo '<div class="form-check">
-        <input class="form-check-input" type="radio" name="crust" id="flexRadioDefault1" onclick="return updateCart('.'$row[\'menu_item_id\'] '.',1);">
+        <input class="form-check-input" type="radio" name="crust" id="flexRadioDefault1" onclick="return updateCart('.$row['menu_item_id'].',1);">
         <label class="form-check-label" for="flexRadioDefault1">
          '. $row["menu_item_name"];
           echo '
