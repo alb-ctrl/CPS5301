c855bcc6bc0d1efbc4e01bc9369e6fed6e77298b
verdecit <verdecit@kean.edu>
2021-12-14 00:41:11 -0500

    n

--- PizzaPlanet/userprofile.php
+++ PizzaPlanet/userprofile.php
@@ -90,7 +90,7 @@ else{
     </nav>
     <div id = "container">
         <div id = "userform">
-        <?php if (isset($_COOKIE['up'])){echo "<span style='color:blue'> ".$_COOKIE['up'] . "</span><br><br>"; unset($_COOKIE['up']); setcookie('up', '', time() - 3600, "/");} ?>
+        <?php if (isset($_COOKIE['up'])){echo "<span style='color:green'> ".$_COOKIE['up'] . "</span><br><br>"; unset($_COOKIE['up']); setcookie('up', '', time() - 3600, "/");} ?>
             <p>Update Information</p><br>
             <form action="update_user.php" method="POST">
                 <ul>
