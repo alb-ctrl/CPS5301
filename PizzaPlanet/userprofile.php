d4366934306414b4cadb6198e3c03228dd0dcdeb
verdecit <verdecit@kean.edu>
2021-12-13 22:35:31 -0500

    a

--- PizzaPlanet/userprofile.php
+++ PizzaPlanet/userprofile.php
@@ -37,7 +37,7 @@ setcookie($cookieid, $user_email, time() + (86400 * 30), "/");
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"  crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"  crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="../rsrc/styles/index_styles.css">
-    <link rel="stylesheet" type="text/css" href="../rsc/styles/userprofile_styles.css">
+    <link rel="stylesheet" type="text/css" href="../rsrc/styles/userprofile_styles.css">
 </head>
 <body>
     <body>
